ecaba1a61c2077dafb5da324eafacf3c01093001
cristian donos <choclomacair@choclomacbookair.local>
2024-12-11 18:50:04 -0300

    pre test

--- ui/src/components/UserCounter.js
+++
@@ -1,8 +0,0 @@
-export default function UserCounter({ clientCount }) {
-    return (
-        <div className="flex items-center text-white">
-            <span className="mr-2">Users Connected:</span>
-            <span className="text-lg font-bold">{clientCount}</span>
-        </div>
-    );
-}
