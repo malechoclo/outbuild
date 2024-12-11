ee1c8496afadc5d639dd72b8e659053dc4ad363f
cristian donos <choclomacair@choclomacbookair.local>
2024-12-11 16:17:34 -0300

    add user counter

--- ui/src/App.js
+++ ui/src/App.js
@@ -18,14 +18,27 @@ function App() {
     const [editDeadline, setEditDeadline] = useState('');
     const [editUrgency, setEditUrgency] = useState(''); // Renamed for editing state
     const [errors, setErrors] = useState({});
+    const [clientCount, setClientCount] = useState(0); // State for client count
+    const [updateClientCount, setUpdateClientCount] = useState(false); // State for client count
+
+
 
     useEffect(() => {
         socket.on('update-board', (newBoard) => setBoard(newBoard));
+        socket.on('client-count', (count) => handleUserCount(count)); // Update client count
 
         return () => {
             socket.disconnect();
         };
     }, []);
+
+    const handleUserCount = (count) => {
+        setClientCount(count)
+        setUpdateClientCount(true);
+        setTimeout(() => {
+            setUpdateClientCount(false);
+        }, 500);
+    }
 
     const validateFields = () => {
         const fieldErrors = {};
@@ -120,9 +133,9 @@ function App() {
             <div className="flex flex-row mx-1 my-5 bg-none p-4 w-full">
                 <div className="w-3/4 flex flex-row">
                     <div className="flex items-start flex-col w-1/4 mr-4">
-                        <label 
-                        className={`mr-4 text-white ${errors.description ? 'text-red-300' : ''}`}
-                        
+                        <label
+                            className={`mr-4 text-white ${errors.description ? 'text-red-300' : ''}`}
+
                         >Description</label>
                         <input
                             type="text"
@@ -169,12 +182,12 @@ function App() {
 
                 <div className='w-1/4 text-white flex flex-row m-auto text-4xl  justify-end items-center mr-5'>
                     <span className='flex justify-end items-center mr-10'>
-                        <span className='flex rounded-full border-solid justify-end items-center mr-2 '>
-                            <svg className="size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
+                        <span className={`flex rounded-full border-solid justify-end items-center mr-2 `}>
+                            <svg className={`size-6 ${updateClientCount ? 'animate-ping' : ''}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                             </svg>
                         </span>
-                        <span className='user-counter text-2xl'>10</span>
+                        <div className="user-counter text-2xl">{clientCount}</div>
                     </span>
 
                     <span className='mr-5 '>Worky</span>
@@ -206,7 +219,7 @@ function App() {
                     />
                 ))}
             </div>
-        </div>
+        </div >
     );
 }
 
