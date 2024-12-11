845abab30c112ac0e0e58409fb5f58ca2edb7975
cristian donos <choclomacair@choclomacbookair.local>
2024-12-11 12:39:56 -0300

    nav ok, drag ok, delete, edit ok

--- ui/src/App.js
+++ ui/src/App.js
@@ -11,55 +11,60 @@ const initialBoard = {
 function App() {
     const [board, setBoard] = useState(initialBoard);
     const [description, setDescription] = useState('');
-    const [urgency, setUrgency] = useState('low');
-    const [deadline, setDeadline] = useState(''); // Track the deadline for the task
+    const [taskUrgency, setTaskUrgency] = useState(''); // Renamed from `urgency` to `taskUrgency`
+    const [deadline, setDeadline] = useState('');
     const [editingTask, setEditingTask] = useState(null);
     const [editContent, setEditContent] = useState('');
-    const [highlightedTasks, setHighlightedTasks] = useState({}); // Track interacting tasks
+    const [editDeadline, setEditDeadline] = useState('');
+    const [editUrgency, setEditUrgency] = useState(''); // Renamed for editing state
+    const [errors, setErrors] = useState({});
 
     useEffect(() => {
         socket.on('update-board', (newBoard) => setBoard(newBoard));
-        socket.on('highlight-task', (activeTasks) => setHighlightedTasks(activeTasks));
 
         return () => {
             socket.disconnect();
         };
     }, []);
 
+    const validateFields = () => {
+        const fieldErrors = {};
+        if (!description.trim()) fieldErrors.description = true;
+        if (!taskUrgency) fieldErrors.taskUrgency = true;
+        if (!deadline) fieldErrors.deadline = true;
+        setErrors(fieldErrors);
+        return Object.keys(fieldErrors).length === 0;
+    };
+
     const addTask = () => {
-        if (!description.trim() || !deadline) return;
+        if (!validateFields()) return;
 
         const newTask = {
             id: Date.now().toString(),
             content: description,
-            createdAt: new Date().toLocaleString(),
-            urgency,
+            urgency: taskUrgency,
             deadline,
+            createdAt: new Date().toLocaleString(),
         };
 
-        const updatedBoard = { ...board, todo: [...board.todo, newTask] };
-        
+        const updatedBoard = Object.assign({}, board);
+        updatedBoard.todo = board.todo.concat(newTask);
         setBoard(updatedBoard);
+
+        // Clear inputs
         setDescription('');
+        setTaskUrgency('');
         setDeadline('');
+        setErrors({});
+
         socket.emit('task-update', updatedBoard);
     };
 
-    const deleteTask = (taskId) => {
-        const updatedBoard = { ...board };
-
-        Object.keys(updatedBoard).forEach((column) => {
-            updatedBoard[column] = updatedBoard[column].filter((task) => task.id !== taskId);
-        });
-
-        setBoard(updatedBoard);
-        socket.emit('task-update', updatedBoard);
-    };
-
-    const editTask = (taskId, content) => {
+    const editTask = (taskId, task) => {
         setEditingTask(taskId);
-        setEditContent(content);
-        notifyInteraction(taskId);
+        setEditContent(task.content);
+        setEditDeadline(task.deadline);
+        setEditUrgency(task.urgency); // Use the renamed setter for editing state
     };
 
     const saveTask = (taskId, newContent, newUrgency, newDeadline) => {
@@ -76,84 +81,107 @@ function App() {
         setBoard(updatedBoard);
         setEditingTask(null);
         setEditContent('');
-        stopInteraction(taskId);
+        setEditDeadline('');
+        setEditUrgency('');
         socket.emit('task-update', updatedBoard);
     };
-
     const handleTaskMove = (taskId, sourceColumn, destinationColumn, newOrderIndex) => {
         const updatedBoard = { ...board };
+
+        // Find the task in the source column and remove it
         const taskIndex = updatedBoard[sourceColumn].findIndex((task) => task.id === taskId);
         const [movedTask] = updatedBoard[sourceColumn].splice(taskIndex, 1);
 
+        // Add the task to the destination column
         updatedBoard[destinationColumn].splice(newOrderIndex, 0, movedTask);
+
+        // Update the state
         setBoard(updatedBoard);
+
+        // Notify other clients via the socket
         socket.emit('task-update', updatedBoard);
-        notifyInteraction(taskId);
     };
 
-    const notifyInteraction = (taskId) => {
-        socket.emit('interact-task', { taskId });
+    const deleteTask = (taskId) => {
+        const updatedBoard = { ...board };
+
+        // Iterate over each column and remove the task
+        Object.keys(updatedBoard).forEach((column) => {
+            updatedBoard[column] = updatedBoard[column].filter((task) => task.id !== taskId);
+        });
+
+        setBoard(updatedBoard); // Update the state
+        socket.emit('task-update', updatedBoard); // Notify other clients
     };
 
-    const stopInteraction = (taskId) => {
-        socket.emit('stop-interact-task', { taskId });
-    };
 
     return (
-        <div className="flex flex-col h-screen bg-gradient-to-r from-cyan-600 to-cyan-700 pb-2 px-1">
-            <div className="flex items-center m-4 bg-cyan-800 p-4">
-                <div className="flex flex-col mr-1 w-1/2">
-                    <label htmlFor="urgency" className="text-white text-sm mb-1">
-                        Description
-                    </label>
-                    <input
-                        type="text"
-                        maxLength={50}
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        placeholder="Task description (max 50 chars)"
-                        className="mr-5 p-2 w-100 border rounded"
-                    />
-                </div>
-                <div className="flex flex-col mr-5">
-                    <label htmlFor="urgency" className="text-white text-sm mb-1">
-                        Urgency
-                    </label>
-                    <select
-                        id="urgency"
-                        value={urgency}
-                        onChange={(e) => setUrgency(e.target.value)}
-                        className="p-2 border rounded bg-white text-black"
-                    >
-                        <option value="high">High</option>
-                        <option value="medium">Medium</option>
-                        <option value="low">Low</option>
-                    </select>
-                </div>
-                <div className="flex flex-col">
-                    <label htmlFor="deadline" className="text-white text-sm mb-1">
-                        Deadline
-                    </label>
-                    <input
-                        type="date"
-                        id="deadline"
-                        value={deadline}
-                        onChange={(e) => setDeadline(e.target.value)}
-                        className="p-2 border rounded bg-white text-black"
-                    />
-                </div>
-                <div className="flex flex-col">
-                    <label htmlFor="deadline" className="text-white text-sm mb-1">
-                        &nbsp;
-                    </label>
-                    <button
-                        onClick={addTask}
-                        className="ml-4 px-4 py-2 bg-gray-200 text-black rounded hover:bg-gray-300"
-                    >
-                        Add Task
-                    </button>
+        <div className="flex flex-col h-screen bg-gradient-to-r from-indigo-600 to-indigo-700 pb-2 px-1">
+            <div className="flex flex-row mx-1 my-5 bg-none p-4 w-full">
+                <div className="w-3/4 flex flex-row">
+                    <div className="flex items-start flex-col w-1/4 mr-4">
+                        <label className="mr-4 text-white">Description</label>
+                        <input
+                            type="text"
+                            maxLength={50}
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            placeholder="Task description (max 50 chars)"
+                            className={`p-2 w-full h-10 border rounded ${errors.description ? 'border-red-500' : ''}`}
+                        />
+                    </div>
+                    <div className="flex items-start flex-col mr-4">
+                        <label className="mr-4 text-white">Urgency</label>
+                        <select
+                            value={taskUrgency}
+                            onChange={(e) => setTaskUrgency(e.target.value)} // Updated with `taskUrgency`
+                            className={`h-10 p-2 w-full border rounded ${errors.taskUrgency ? 'border-red-500' : ''}`}
+                        >
+                            <option value="">Select urgency</option>
+                            <option value="low">Low</option>
+                            <option value="medium">Medium</option>
+                            <option value="high">High</option>
+                        </select>
+                    </div>
+                    <div className="flex items-start flex-col mr-4">
+                        <label className="mr-4 text-white">Deadline</label>
+                        <input
+                            type="date"
+                            value={deadline}
+                            onChange={(e) => setDeadline(e.target.value)}
+                            className={`h-10 p-2 w-full border rounded ${errors.deadline ? 'border-red-500' : ''}`}
+                        />
+                    </div>
+                    <div className="flex items-start flex-col">
+                        <label className="mr-4 text-white">&nbsp;</label>
+                        <button
+                            onClick={addTask}
+                            className="inline-block rounded bg-transparent px-6 pb-2 pt-2.5 text-1xl  uppercase  text-white border-solid border-1 border-white animate-pulse"
+                        >
+                            Add Task
+                        </button>
+                    </div>
                 </div>
 
+
+                <div className='w-1/4 text-white flex flex-row m-auto text-4xl  justify-end items-center mr-5'>
+                    <span className='flex justify-end items-center mr-10'>
+                        <span className='flex rounded-full border-solid justify-end items-center mr-2 '>
+                            <svg className="size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+                            </svg>
+                        </span>
+                        <span className='user-counter text-2xl'>10</span>
+                    </span>
+
+                    <span className='mr-5 '>Worky</span>
+                    <span class="">
+                        <svg className='h-16 w-16"' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122" />
+                        </svg>
+                    </span>
+
+                </div>
             </div>
             <div className="flex flex-grow overflow-hidden">
                 {['todo', 'inProgress', 'done'].map((column) => (
@@ -161,16 +189,18 @@ function App() {
                         key={column}
                         title={column}
                         tasks={board[column]}
-                        highlightedTasks={highlightedTasks}
-                        onTaskMove={handleTaskMove}
-                        onTaskDelete={deleteTask}
-                        onTaskEdit={editTask}
+                        onTaskMove={handleTaskMove} // Pass the newly defined function
+                        onTaskDelete={(taskId) => deleteTask(taskId)}
+                        onTaskEdit={(taskId, task) => editTask(taskId, task)}
                         editingTask={editingTask}
                         editContent={editContent}
                         setEditContent={setEditContent}
-                        saveTask={saveTask}
+                        editDeadline={editDeadline}
+                        setEditDeadline={setEditDeadline}
+                        editUrgency={editUrgency}
+                        setUrgency={setEditUrgency}
+                        saveTask={(id, content, urgency, deadline) => saveTask(id, content, urgency, deadline)}
                     />
-
                 ))}
             </div>
         </div>
--- ui/src/components/Column.js
+++ ui/src/components/Column.js
@@ -1,60 +1,139 @@
+import { useState } from "react";
+
 export default function Column({
     title,
     tasks = [],
-    highlightedTasks,
     onTaskMove,
     onTaskDelete,
     onTaskEdit,
     editingTask,
     editContent,
     setEditContent,
+    editDeadline,
+    setEditDeadline,
+    editUrgency,
+    setUrgency,
     saveTask,
 }) {
+    const handleDragStart = (e, taskId) => {
+        e.dataTransfer.setData('taskId', taskId);
+        e.dataTransfer.setData('sourceColumn', title);
+    };
+    const [isDeleting, setIsDeleting] = useState(null)
+    const handleDragOver = (e) => e.preventDefault();
+    const handleDelete = (id) => {
+        setIsDeleting(id)
+        setTimeout(() => {
+            onTaskDelete(id);
+        }, 500);
+    }
+    const handleDrop = (e) => {
+        e.preventDefault();
+        const taskId = e.dataTransfer.getData('taskId');
+        const sourceColumn = e.dataTransfer.getData('sourceColumn');
+        const destinationColumn = title;
+
+        if (sourceColumn !== destinationColumn) {
+            onTaskMove(taskId, sourceColumn, destinationColumn, tasks.length);
+        }
+    };
+
+    const getUrgencyColor = (urgency) => {
+        if (urgency === 'high') return 'bg-red-500';
+        if (urgency === 'medium') return 'bg-orange-300';
+        return 'bg-green-500';
+    };
+
     return (
         <div
-            className="w-1/3 p-4 border border-gray-300 mx-3 overflow-y-auto h-full"
+            className="w-1/3 p-6 pt-0 border border-transparent mx-1 overflow-y-auto h-full bg-black rounded-2xl "
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
         >
-            <h2 className="text-xl font-bold mb-4 sticky top-0 bg-white z-10 p-2 border-b">
+            <h2 className="text-xl font-bold  sticky top-0 bg-black z-10 p-4 bg-black text-white">
                 {title.toUpperCase()}
             </h2>
             <div>
-                {tasks}
-                {tasks.length && tasks.map((task) => (
+                {tasks.map((task) => (
                     <div
                         key={task.id}
-                        className={`p-4 m-2 rounded bg-gray-800 hover:shadow cursor-grab ${highlightedTasks[task.id] ? 'opacity-50' : ''
-                            }`}
+                        draggable
+                        onDragStart={(e) => handleDragStart(e, task.id)}
+                        className={`p-4 m-2 rounded bg-gray-800 cursor-grab opacity-80 hover:opacity-100 ${isDeleting === task.id ? 'animate-ping' : ''}`}
                     >
-                        {editingTask === task.id ? (
-                            <div className="mt-3">
-                                {editContent.map(([key, value], index) => (
-                                    <div key={index} className="mt-2">
-                                        <label className="text-white text-sm">{`Edit ${key}`}</label>
-                                        <input
-                                            type={key === 'deadline' ? 'date' : 'text'}
-                                            value={value}
-                                            onChange={(e) => {
-                                                const updated = [...editContent];
-                                                updated[index][1] = e.target.value;
-                                                setEditContent(updated);
-                                            }}
-                                            className="p-2 mt-1 w-full border rounded bg-white text-black"
-                                        />
-                                    </div>
-                                ))}
+
+                        <div className="flex justify-between items-center ">
+                            <div className="flex items-center gap-2">
+                                <span className={`w-3 h-3 rounded-full ${getUrgencyColor(task.urgency)}`} />
+                                <small className="text-white">{task.deadline}</small>
+                            </div>
+                            <div className="flex items-center gap-2">
                                 <button
-                                    onClick={() =>
-                                        saveTask(task.id, editContent)
-                                    }
+                                    onClick={() => onTaskEdit(task.id, task)}
+                                    className="text-blue-300 hover:text-blue-500"
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                                    </svg>
+
+                                </button>
+                                <button
+                                    onClick={() => handleDelete(task.id)}
+                                    className="text-red-300 hover:text-red-500 flex flex-row items-center "
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4 mr-1">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+
+                        <div className="text-white">{task.content}</div>
+                        <div className="text-gray-400 text-sm">Created: {task.createdAt}</div>
+
+                        {editingTask === task.id && (
+                            <div className="mt-3">
+                                {/* Edit Content */}
+                                <label className="text-white text-sm">Edit Content</label>
+                                <input
+                                    type="text"
+                                    value={editContent}
+                                    onChange={(e) => setEditContent(e.target.value)}
+                                    className="p-2 mt-1 w-full border rounded"
+                                />
+
+                                {/* Edit Urgency */}
+                                <label className="text-white text-sm mt-3">Edit Urgency</label>
+                                <select
+                                    value={editUrgency}
+                                    onChange={(e) => setUrgency(e.target.value)}
+                                    className="p-2 mt-1 w-full border rounded bg-white text-black"
+                                >
+                                    <option value="high">High</option>
+                                    <option value="medium">Medium</option>
+                                    <option value="low">Low</option>
+                                </select>
+
+                                {/* Edit Deadline */}
+                                <label className="text-white text-sm mt-3">Edit Deadline</label>
+                                <input
+                                    type="date"
+                                    value={editDeadline}
+                                    onChange={(e) => setEditDeadline(e.target.value)}
+                                    className="p-2 mt-1 w-full border rounded bg-white text-black"
+                                />
+
+                                {/* Save Button */}
+                                <button
+                                    onClick={() => saveTask(task.id, editContent, editUrgency, editDeadline)}
                                     className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
                                 >
                                     Save
                                 </button>
                             </div>
-                        ) : (
-                            <p className="text-gray-100 mt-3">{task.content}</p>
                         )}
                     </div>
+
                 ))}
             </div>
         </div>
