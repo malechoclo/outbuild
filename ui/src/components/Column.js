eed68351ac5246aee839dd6ca62935c1a88d12de
cristian donos <choclomacair@choclomacbookair.local>
2024-12-12 09:32:36 -0300

    drag-editin-ok

--- ui/src/components/Column.js
+++ ui/src/components/Column.js
@@ -68,8 +68,8 @@ export default function Column({
 
     const getTaskClass = (taskId) => {
         return isEditingTasks(taskId)
-            ? 'opacity-50 editing'
-            : 'bg-gray-800 opacity-100';
+            ? 'opacity-60 editing'
+            : 'opacity-100';
     };
 
 
@@ -77,25 +77,24 @@ export default function Column({
         saveTask(taskId, newContent, newUrgency, newDeadline);
         onTaskInteractionEnd(taskId);
     };
-
+    
     return (
         <div
-            className="w-1/3 p-6 pt-0 border border-transparent mx-1 overflow-y-auto h-full bg-black rounded-2xl "
+            className={`w-1/3 p-2 pt-0 border border-transparent mx-1 overflow-y-auto h-full bg-gray-100 rounded-2xl `}
             onDragOver={handleDragOver}
             onDrop={handleDrop}
         >
-            <h2 className="text-xl font-bold sticky top-0 bg-black z-10 p-4 bg-black text-white">
+            <h2 className="text-xl font-bold sticky top-0  bg-gray-100 z-10 p-4 bg-black text-indigo-800">
                 {title.toUpperCase()}
             </h2>
             <div>
                 {tasks.map((task) => (
                     <div
                         key={task.id}
-                        draggable={isEditingTasks(task.id)}
+                        draggable={!getTaskClass(task.id).includes("editing")}
                         onDragStart={(e) => handleDragStart(e, task.id)}
-                        className={`p-4 m-2 rounded bg-gray-800 cursor-grab opacity-80 hover:opacity-100 ${getTaskClass(task.id)} ${isDeleting === task.id ? 'animate-ping' : ''}`}
+                        className={`p-4 m-2 rounded-2xl bg-indigo-800 cursor-grab opacity-90 hover:opacity-100 ${getTaskClass(task.id)} ${isDeleting === task.id ? 'animate-ping' : ''}`}
                     >
-
                         <div className="flex justify-between items-center ">
                             <div className="flex items-center gap-2">
                                 <span className={`w-3 h-3 rounded-full ${getUrgencyColor(task.urgency)}`} />
@@ -107,7 +106,7 @@ export default function Column({
                                         onClick={() => handleEditStart(task.id, task)}
                                         className="text-blue-300 hover:text-blue-500"
                                     >
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
                                         </svg>
 
@@ -116,7 +115,7 @@ export default function Column({
                                         onClick={() => handleDelete(task.id)}
                                         className="text-red-300 hover:text-red-500 flex flex-row items-center "
                                     >
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4 mr-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 mr-1">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                         </svg>
                                     </button>
