21b1befbe30eb56e3f4e2039d853f464509d739e
cristian donos <choclomacair@choclomacbookair.local>
2024-12-11 23:55:28 -0300

    wip

--- ui/src/components/Column.js
+++ ui/src/components/Column.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export default function Column({
     title,
-    tasks = [],
+    tasks,
     onTaskMove,
     onTaskDelete,
     onTaskEdit,
@@ -14,19 +14,17 @@ export default function Column({
     editUrgency,
     setUrgency,
     saveTask,
+    onTaskInteractionStart,
+    onTaskInteractionEnd,
+    highlightedTasks,
+    socket
 }) {
+    const [isDeleting, setIsDeleting] = useState(null)
     const handleDragStart = (e, taskId) => {
         e.dataTransfer.setData('taskId', taskId);
         e.dataTransfer.setData('sourceColumn', title);
     };
-    const [isDeleting, setIsDeleting] = useState(null)
     const handleDragOver = (e) => e.preventDefault();
-    const handleDelete = (id) => {
-        setIsDeleting(id)
-        setTimeout(() => {
-            onTaskDelete(id);
-        }, 500);
-    }
     const handleDrop = (e) => {
         e.preventDefault();
         const taskId = e.dataTransfer.getData('taskId');
@@ -38,6 +36,27 @@ export default function Column({
         }
     };
 
+    const getTaskClass = (taskId) => {
+        return editingTasks[taskId] && editingTasks[taskId] !== socketId
+            ? 'bg-red-500 cursor-not-allowed'
+            : 'bg-gray-800 cursor-grab';
+    };
+
+    const canEditTask = (taskId) => {
+        return editingTasks[taskId] === socketId || !editingTasks[taskId];
+    };
+
+    const editingTasks = (taskId) => { }
+
+    const handleEditStart = (id, task) => {
+        onTaskEdit(id, task);
+        onTaskInteractionStart(id);
+        console.log("Column", editingTask)
+    };
+    const handleSaveTask = (taskId, newContent, newUrgency, newDeadline) => {
+        saveTask(taskId, newContent, newUrgency, newDeadline);
+        onTaskInteractionEnd(taskId);
+    };
     const getUrgencyColor = (urgency) => {
         if (urgency === 'high') return 'bg-red-500';
         if (urgency === 'medium') return 'bg-orange-300';
@@ -47,10 +66,9 @@ export default function Column({
     return (
         <div
             className="w-1/3 p-6 pt-0 border border-transparent mx-1 overflow-y-auto h-full bg-black rounded-2xl "
-            onDragOver={handleDragOver}
             onDrop={handleDrop}
         >
-            <h2 className="text-xl font-bold  sticky top-0 bg-black z-10 p-4 bg-black text-white">
+            <h2 className="text-xl font-bold sticky top-0 bg-black z-10 p-4 bg-black text-white">
                 {title.toUpperCase()}
             </h2>
             <div>
@@ -59,7 +77,8 @@ export default function Column({
                         key={task.id}
                         draggable
                         onDragStart={(e) => handleDragStart(e, task.id)}
-                        className={`p-4 m-2 rounded bg-gray-800 cursor-grab opacity-80 hover:opacity-100 ${isDeleting === task.id ? 'animate-ping' : ''}`}
+                        onDragEnd={(e) => handleDragOver(e)}
+                        className={`p-4 m-2 rounded bg-gray-800 cursor-grab opacity-80 hover:opacity-100 ${getTaskClass(task.id)} ${isDeleting === task.id ? 'animate-ping' : ''}`}
                     >
 
                         <div className="flex justify-between items-center ">
@@ -67,25 +86,28 @@ export default function Column({
                                 <span className={`w-3 h-3 rounded-full ${getUrgencyColor(task.urgency)}`} />
                                 <small className="text-white">{task.deadline}</small>
                             </div>
-                            <div className="flex items-center gap-2">
-                                <button
-                                    onClick={() => onTaskEdit(task.id, task)}
-                                    className="text-blue-300 hover:text-blue-500"
-                                >
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
-                                    </svg>
+                            {
+                                !getTaskClass(task.id)?.includes('editing') ? <div className="flex items-center gap-2">
+                                    <button
+                                        onClick={() => handleEditStart(task.id, task)}
+                                        className="text-blue-300 hover:text-blue-500"
+                                    >
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                                        </svg>
 
-                                </button>
-                                <button
-                                    onClick={() => handleDelete(task.id)}
-                                    className="text-red-300 hover:text-red-500 flex flex-row items-center "
-                                >
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4 mr-1">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                    </svg>
-                                </button>
-                            </div>
+                                    </button>
+                                    <button
+                                        onClick={() => handleDelete(task.id)}
+                                        className="text-red-300 hover:text-red-500 flex flex-row items-center "
+                                    >
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4 mr-1">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                        </svg>
+                                    </button>
+                                </div> : <></>
+                            }
+
                         </div>
 
                         <div className="text-white">{task.content}</div>
@@ -125,9 +147,10 @@ export default function Column({
 
                                 {/* Save Button */}
                                 <button
-                                    onClick={() => saveTask(task.id, editContent, editUrgency, editDeadline)}
+                                    onClick={() => handleSaveTask(task.id, editContent, editUrgency, editDeadline)}
                                     className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
                                 >
+
                                     Save
                                 </button>
                             </div>
